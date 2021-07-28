9b29d6e54b7e6644994e471476d9836c7ce7495a
Rafael <rafael.gcaldas01@gmail.com>
2021-07-28 14:28:40 -0300

    Login page finished

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -38,20 +39,28 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             Positioned(
-              bottom: size.height * 0.18,
+              bottom: size.height * 0.05,
               left: 0,
               right: 0,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset( AppImages.logomini),
+                  Image.asset(AppImages.logomini),
                   Padding(
-                    padding: const EdgeInsets.only(top: 16, left: 50, right: 50),
-                    child: Text(
-                      "Organize seus boletos em um só lugar",
-                      textAlign: TextAlign.center,
-                      style: TextStyles.titleHome
+                    padding:
+                        const EdgeInsets.only(top: 16, left: 50, right: 50),
+                    child: Text("Organize seus boletos em um só lugar",
+                        textAlign: TextAlign.center,
+                        style: TextStyles.titleHome),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 36),
+                    child: SocialLoginButon(
+                      onTap: () {
+                        print("clicou");
+                      }
                     ),
                   )
                 ],
