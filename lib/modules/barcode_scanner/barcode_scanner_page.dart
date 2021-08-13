251513a18d8c07cc7a5ed694531cd46c034f9cd1
Rafael <rafael.gcaldas01@gmail.com>
2021-08-12 22:32:04 -0300

    Divider vertical widget

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -42,11 +43,15 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
             )),
         ],
       ),
-      bottomNavigationBar: Row(
-        children: [
-          Expanded(child: LabelButton(label: "Inserir código do boleto", onPressed: (){})),
-          Expanded(child: LabelButton(label: "Adicionar da galeria", onPressed: (){}))
-        ],
+      bottomNavigationBar: Container(
+        height: 56,
+        child: Row(
+          children: [
+            Expanded(child: LabelButton(label: "Inserir código do boleto", onPressed: (){})),
+            DividerVerticalWidget(),
+            Expanded(child: LabelButton(label: "Adicionar da galeria", onPressed: (){}))
+          ],
+        ),
       ),
     );
   }
