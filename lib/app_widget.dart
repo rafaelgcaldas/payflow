e3665af33f4acc6f21c3ee65112acc11b0008c0f
Rafael <rafael.gcaldas01@gmail.com>
2021-08-13 22:22:29 -0300

    Set orientation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
@@ -7,21 +8,27 @@ import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        primaryColor: AppColors.primary,
-      ),
-      initialRoute: "/splash",
-      routes: {
-        "/splash" : (context) => SplashPage(),
-        "/home" : (context) => HomePage(),
-        "/login" : (context) => LoginPage(),
-        "/barcode_scanner" : (context) => BarcodeScannerPage(),
-      }
-    );
+        title: 'Pay Flow',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          primaryColor: AppColors.primary,
+        ),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+        });
   }
 }
